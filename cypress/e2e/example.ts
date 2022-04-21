e502e75846120ed0826250d3431fc3a1208639d5
Pedro <pedro-gomes17@hotmail.com>
2022-04-21 12:17:56 -0300

    config: clone wongames repo and added cypress configurations

---
+++ cypress/e2e/example.ts
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+///<reference path="../support/index.d.ts" />
+
+describe('Cypress TS', () => {
+  it('should go to google', () => {
+    cy.google()
+  })
+  it('should test', () => {
+    cy.visit('https://willianjusten.com.br')
+
+    cy.findByTitle(/Mudar o tema/i).click()
+    cy.get('.light').should('exist')
+  })
+})
---
+++ cypress/support/index.d.ts
@@ -0,0 +1,13 @@
+// load type definitions from Cypress module
+/// <reference types="cypress" />
+
+declare namespace Cypress {
+  interface Chainable<> {
+    /**
+     * Custom command to visit google page.
+     * @example cy.google()
+     */
+
+    google(): Chainable<Window>
+  }
+}
