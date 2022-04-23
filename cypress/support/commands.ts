a8171a210870616fa08afb728700ee32963bacc1
Pedro <pedro-gomes17@hotmail.com>
2022-04-23 11:07:50 -0300

    test: added new command shouldRenderShowcase

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -46,3 +46,19 @@ Cypress.Commands.add('shouldRenderBanner', () => {
     cy.findByRole('link', { name: /browse games/i })
   })
 })
+
+Cypress.Commands.add('shouldRenderShowcase', ({ name, highlight = false }) => {
+  cy.get(`[data-cy="${name}"]`).within(() => {
+    cy.findByRole('heading', { name }).should('exist')
+
+    cy.get(`[data-cy="highlight"]`).should(highlight ? 'exist' : 'not.exist')
+
+    if (highlight) {
+      cy.get(`[data-cy="highlight"]`).within(() => {
+        cy.findByRole('link').should('have.attr', 'href')
+      })
+    }
+
+    // cy.get(`[data-cy="games-card"]`).should('exist')
+  })
+})
--- cypress/support/index.d.ts
+++ cypress/support/index.d.ts
@@ -1,6 +1,10 @@
 // load type definitions from Cypress module
 /// <reference types="cypress" />
 
+type ShowcaseAttributes = {
+  name: string
+  highlight?: boolean
+}
 declare namespace Cypress {
   interface Chainable<> {
     /**
@@ -11,10 +15,17 @@ declare namespace Cypress {
     google(): Chainable<Window>
 
     /**
-     * Custom command to visit google page.
+     * Custom command to check banner in page.
      * @example cy.shouldRenderBanner()
      */
 
     shouldRenderBanner(): Chainable<Element>
+
+    /**
+     * Custom command to check showcase in page
+     * @example cy.shouldRenderShowcase()
+     */
+
+    shouldRenderShowcase(attrs: ShowcaseAttributes): Chainable<Element>
   }
 }
