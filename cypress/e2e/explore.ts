092732b2e07bdc229ac149b32cf892fb1944f69a
Pedro <pedro-gomes17@hotmail.com>
2022-04-24 15:34:54 -0300

    test: added two new tests: should order by price range and should filter by platform and genre to explorer page

--- cypress/e2e/explore.ts
+++ cypress/e2e/explore.ts
@@ -50,7 +50,7 @@ describe('Explore page', () => {
       })
   })
 
-  it('should order by price range', () => {
+  it.skip('should order by price range', () => {
     cy.findByText(/highest to lowest/i).click()
     cy.findByText(/free/i).click()
     cy.location('href').should('contain', 'price_lte=0')
@@ -92,5 +92,30 @@ describe('Explore page', () => {
       .within(() => {
         cy.shouldBeLessThan(500)
       })
+  })
+
+  it.skip('should filter by platform and genre', () => {
+    cy.findByText(/windows/i).click()
+    cy.location('href').should('contain', 'platforms=windows')
+
+    cy.findByText(/linux/i).click()
+    cy.location('href').should('contain', 'platforms=linux')
+
+    cy.findByText(/mac os/i).click()
+    cy.location('href').should('contain', 'platforms=mac')
+
+    cy.findByText(/action/i).click()
+    cy.location('href').should('contain', 'categories=action')
+  })
+
+  it('should return empty when no games match', () => {
+    cy.visit('/games') //needed to clear url params
+
+    cy.findByText(/free/i).click()
+    cy.findByText(/linux/i).click()
+    cy.findByText(/sports/i).click()
+
+    cy.getByDataCy('games-card').should('not.exist')
+    cy.findByText(/We didn't find any games with this filter/).should('exist')
   })
 })
