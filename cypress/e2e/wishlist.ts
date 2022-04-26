83f3ff2234bc5cd3681185922e2455b569ae18d5
Pedro <pedro-gomes17@hotmail.com>
2022-04-25 21:46:03 -0300

    test: added new test to wishlist page

---
+++ cypress/e2e/wishlist.ts
@@ -0,0 +1,28 @@
+// eslint-disable-next-line @typescript-eslint/triple-slash-reference
+///<reference path="../support/index.d.ts" />
+
+describe('Wishlist page', () => {
+  it('should add and remove games from wishlist', () => {
+    cy.visit('/wishlist')
+
+    cy.signIn()
+    cy.wait(1000)
+
+    cy.findByText(/Your wishlist is empty/i).should('exist')
+
+    cy.getByDataCy('games-card')
+      .eq(0)
+      .within(() => {
+        cy.findAllByLabelText(/add to wishlist/i).click()
+      })
+
+    cy.wait(300)
+    cy.getByDataCy('wishlist').within(() => {
+      cy.getByDataCy('games-card').should('have.length', 1)
+    })
+
+    cy.getByDataCy('wishlist').within(() => {
+      cy.findAllByLabelText(/remove from wishlist/i).click()
+    })
+  })
+})
